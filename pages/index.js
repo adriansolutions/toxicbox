8469d143703aacb24a761890dd4d255ec22057dd
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-21 16:35:28 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-import ToxicBox from "../components/ToxicBox";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <ToxicBox />;
+  useEffect(() => {
+    // LOAD YOUR EXACT SCRIPT
+    const script = document.createElement("script");
+    script.src = "/toxbox.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <>
+      {/* YOUR EXACT HTML GOES HERE */}
+      <div
+        dangerouslySetInnerHTML={{
+          __html: require("fs").readFileSync(
+            process.cwd() + "/public/index.html",
+            "utf8"
+          ),
+        }}
+      />
+    </>
+  );
 }
