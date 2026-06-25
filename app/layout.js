a130dab961f977acfc5eed8a35e09714081af9cd
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-25 20:07:47 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "BlueChat",
   description: "Live Chat App",
+  <meta
+  name="mobile-web-app-capable"
+  content="yes"
+/>
 };
 
 export default function RootLayout({ children }) {
