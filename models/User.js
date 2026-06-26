73591c8f639e34a255b11daf3c25f628f4163fd8
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-26 21:51:27 +0800

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const UserSchema =
+new mongoose.Schema({
+
+username: {
+  type: String,
+  unique: true,
+},
+
+userId: String,
+
+password: String,
+
+avatar: String,
+
+online: {
+  type: Boolean,
+  default: false,
+},
+
+});
+
+export default
+mongoose.models.User ||
+mongoose.model(
+"User",
+UserSchema
+);
