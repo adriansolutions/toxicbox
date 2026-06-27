5f5cf003dd83f8d1dd2fc5447f43f36ca73c530f
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-27 21:16:52 +0800

    Update route.js

--- app/api/accept-friend-requests/route.js
+++ app/api/accept-friend-requests/route.js
@@ -1,47 +1,70 @@
 import connectDB from "../../../lib/mongodb";
+
 import FriendRequest from "../../../models/FriendRequest";
-import User from "../../../models/User";
 
 export async function POST(req) {
+
   try {
+
     await connectDB();
 
-    const { currentUserId, friendUserId } = await req.json();
+    const {
+      currentUserId,
+      fromUserId,
+    } = await req.json();
 
-    // 1. Remove request
+    const request =
+      await FriendRequest.findOne({
+
+        toUserId:
+          currentUserId,
+
+        fromUserId,
+
+      });
+
+    if (!request) {
+
+      return Response.json({
+        success: false,
+        message: "Request not found",
+      });
+
+    }
+
+    // DELETE REQUEST
     await FriendRequest.deleteOne({
-      fromUserId: friendUserId,
-      toUserId: currentUserId,
+      _id: request._id,
     });
 
-    // 2. Add to BOTH users friend lists
-    await User.updateOne(
-      { userId: currentUserId },
-      { $addToSet: { friends: friendUserId } }
-    );
-
-    await User.updateOne(
-      { userId: friendUserId },
-      { $addToSet: { friends: currentUserId } }
-    );
-
-    // 3. Get updated friends
-    const user = await User.findOne({ userId: currentUserId });
-
-    const friends = await User.find({
-      userId: { $in: user.friends || [] },
-    }).select("username userId avatar");
-
     return Response.json({
+
       success: true,
-      friends,
+
+      friend: {
+
+        username:
+          request.fromUsername,
+
+        userId:
+          request.fromUserId,
+
+        avatar:
+          request.fromAvatar || "",
+
+      },
+
     });
 
   } catch (err) {
+
     console.log(err);
+
     return Response.json({
       success: false,
       message: "Server error",
     });
+
   }
+
 }
