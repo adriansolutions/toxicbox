900a3be2755a1c55d8d699b022175ccff14cf6c5
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-27 21:17:28 +0800

    Update route.js

--- app/api/get-friend-requests/route.js
+++ app/api/get-friend-requests/route.js
@@ -4,22 +4,16 @@ import connectDB from "../../../lib/mongodb";
 import FriendRequest from "../../../models/FriendRequest";
 
 export async function GET(req) {
+
   try {
+
     await connectDB();
 
-    const { searchParams } = new URL(req.url);
+    const { searchParams } =
+      new URL(req.url);
 
     const userId =
       searchParams.get("userId");
-
-    if (!userId) {
-
-      return Response.json({
-        success: false,
-        message: "Missing userId",
-      });
-
-    }
 
     const requests =
       await FriendRequest.find({
@@ -37,8 +31,9 @@ export async function GET(req) {
 
     return Response.json({
       success: false,
-      message: "Server error",
+      requests: [],
     });
 
   }
+
 }
