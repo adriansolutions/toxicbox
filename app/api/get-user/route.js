0ba4eb286c0bf1fef9931c26c2655fb313523796
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-27 22:25:29 +0800

    Create route.js

---
+++ app/api/get-user/route.js
@@ -0,0 +1,47 @@
+import connectDB from "../../../lib/mongodb";
+import User from "../../../models/User";
+
+export const dynamic =
+  "force-dynamic";
+
+export async function GET(req) {
+
+  try {
+
+    await connectDB();
+
+    const { searchParams } =
+      new URL(req.url);
+
+    const userId =
+      searchParams.get("userId");
+
+    const user =
+      await User.findOne({
+        userId,
+      }).lean();
+
+    if (!user) {
+
+      return Response.json({
+        success: false,
+      });
+
+    }
+
+    return Response.json({
+      success: true,
+      user,
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    return Response.json({
+      success: false,
+    });
+
+  }
+
+}
