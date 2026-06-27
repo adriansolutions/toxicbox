1fe2b63d1285ba4eb294990f13c0538b7a19c2dd
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-27 21:18:22 +0800

    Update route.js

--- app/api/send-friend-request/route.js
+++ app/api/send-friend-request/route.js
@@ -2,7 +2,9 @@ import connectDB from "../../../lib/mongodb";
 import FriendRequest from "../../../models/FriendRequest";
 
 export async function POST(req) {
+
   try {
+
     await connectDB();
 
     const {
@@ -12,28 +14,31 @@ export async function POST(req) {
       avatar,
     } = await req.json();
 
-    // prevent duplicate requests
-    const existing = await FriendRequest.findOne({
-      fromUserId,
-      toUserId,
-    });
+    // CHECK EXISTING
+    const existing =
+      await FriendRequest.findOne({
+        fromUserId,
+        toUserId,
+      });
 
     if (existing) {
+
       return Response.json({
         success: false,
         message: "Request already sent",
       });
+
     }
 
-    // save request
+    // SAVE REQUEST
     await FriendRequest.create({
+
       fromUserId,
+      fromUsername: username,
+      fromAvatar: avatar || "",
+
       toUserId,
 
-      // IMPORTANT
-      userId: fromUserId,
-      username,
-      avatar,
     });
 
     return Response.json({
@@ -41,11 +46,14 @@ export async function POST(req) {
     });
 
   } catch (err) {
+
     console.log(err);
 
     return Response.json({
       success: false,
       message: "Server error",
     });
+
   }
+
 }
