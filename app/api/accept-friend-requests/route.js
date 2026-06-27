8f4fc5be938e716726b2a11aa5f8f4ef849ed132
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-27 21:37:06 +0800

    Update route.js

--- app/api/accept-friend-requests/route.js
+++ app/api/accept-friend-requests/route.js
@@ -1,101 +1,50 @@
 import connectDB from "../../../lib/mongodb";
 
-import User from "../../../models/User";
-
 import FriendRequest from "../../../models/FriendRequest";
+
+import Friend from "../../../models/Friend";
+
+export const dynamic =
+  "force-dynamic";
 
 export async function POST(req) {
 
-try {
+  try {
 
-await connectDB();
+    await connectDB();
 
-const {
-  currentUserId,
-  fromUserId,
-} = await req.json();
+    const {
+      currentUserId,
+      fromUserId,
+    } = await req.json();
 
-// FIND USERS
-const currentUser =
-  await User.findOne({
-    userId: currentUserId,
-  });
+    // CREATE FRIEND
+    await Friend.create({
+      users: [
+        currentUserId,
+        fromUserId,
+      ],
+    });
 
-const fromUser =
-  await User.findOne({
-    userId: fromUserId,
-  });
+    // REMOVE REQUEST
+    await FriendRequest.deleteOne({
+      fromUserId,
+      toUserId:
+        currentUserId,
+    });
 
-if (
-  !currentUser ||
-  !fromUser
-) {
+    return Response.json({
+      success: true,
+    });
 
-  return Response.json({
-    success: false,
-    message: "User not found",
-  });
+  } catch (err) {
 
-}
+    console.log(err);
 
-// CHECK IF ALREADY FRIENDS
-const alreadyFriend =
-  currentUser.friends?.find(
-    (f) =>
-      f.userId === fromUserId
-  );
+    return Response.json({
+      success: false,
+    });
 
-if (!alreadyFriend) {
-
-  // ADD TO CURRENT USER
-  currentUser.friends.push({
-    username:
-      fromUser.username,
-
-    userId:
-      fromUser.userId,
-
-    avatar:
-      fromUser.avatar || "",
-  });
-
-  // ADD TO OTHER USER
-  fromUser.friends.push({
-    username:
-      currentUser.username,
-
-    userId:
-      currentUser.userId,
-
-    avatar:
-      currentUser.avatar || "",
-  });
-
-  await currentUser.save();
-
-  await fromUser.save();
-
-}
-
-// REMOVE FRIEND REQUEST
-await FriendRequest.deleteOne({
-  fromUserId,
-  toUserId: currentUserId,
-});
-
-return Response.json({
-  success: true,
-});
-
-} catch (err) {
-
-console.log(err);
-
-return Response.json({
-  success: false,
-  message: "Server error",
-});
-
-}
+  }
 
 }
