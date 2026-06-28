8f660c74e2cf261e9fad4086ca532a02052c0292
adriansolutions <144000340+adriansolutions@users.noreply.github.com>
2026-06-29 01:17:11 +0800

    Update route.js

--- app/api/get-user/route.js
+++ app/api/get-user/route.js
@@ -1,47 +1,97 @@
 import connectDB from "../../../lib/mongodb";
 import User from "../../../models/User";
 
-export const dynamic =
-  "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export async function GET(req) {
 
-  try {
+try {
 
-    await connectDB();
+await connectDB();
 
-    const { searchParams } =
-      new URL(req.url);
+const { searchParams } =
+  new URL(req.url);
 
-    const userId =
-      searchParams.get("userId");
+const userId =
+  searchParams.get("userId");
 
-    const user =
-      await User.findOne({
-        userId,
-      }).lean();
+if (!userId) {
 
-    if (!user) {
+  return Response.json({
+    success: false,
+  });
 
-      return Response.json({
-        success: false,
-      });
+}
 
-    }
+const user =
+  await User.findOne({
+    userId,
+  }).lean();
 
-    return Response.json({
-      success: true,
-      user,
-    });
+if (!user) {
 
-  } catch (err) {
+  return Response.json({
+    success: false,
+  });
 
-    console.log(err);
+}
 
-    return Response.json({
-      success: false,
-    });
+return Response.json({
+  success: true,
 
-  }
+  user: {
+    username:
+      user.username || "",
+
+    userId:
+      user.userId || "",
+
+    avatar:
+      user.avatar || "",
+
+    banner:
+      user.banner || "",
+
+    bio:
+      user.bio || "",
+
+    hometown:
+      user.hometown || "",
+
+    birthday:
+      user.birthday || "",
+
+    status:
+      user.status || "",
+
+    language:
+      user.language || "",
+
+    gender:
+      user.gender || "",
+
+    work:
+      user.work || "",
+
+    education:
+      user.education || "",
+
+    hobbies:
+      user.hobbies || "",
+
+    friends:
+      user.friends || [],
+  },
+});
+
+} catch (err) {
+
+console.log(err);
+
+return Response.json({
+  success: false,
+});
+
+}
 
 }
